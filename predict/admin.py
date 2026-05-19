9190d83a0ba93a56266d6b5ae79013a3757e4535
ridayasth4 <ridaya350@gmail.com>
2026-05-19 20:31:01 +0545

    added extra feature columns

--- predict/admin.py
+++ predict/admin.py
@@ -3,7 +3,7 @@ from .models import PredictionLog
 
 @admin.register(PredictionLog)
 class PredictionLogAdmin(admin.ModelAdmin):
-    list_display = ('url', 'prediction', 'confidence','domain_age_days','ssl_valid','using_shortener', 'created_at')
+    list_display = ('url', 'prediction', 'confidence','domain_age_days','ssl_valid','using_shortener','url_length', 'num_subdomains', 'contains_keyword', 'has_ip', 'created_at')
     list_filter = ('prediction', 'created_at')
     search_fields = ('url', 'task_id')
     readonly_fields = ('task_id', 'url', 'prediction', 'confidence', 'features', 'created_at')
--- predict/models.py
+++ predict/models.py
@@ -19,6 +19,23 @@ class PredictionLog(models.Model):
     @property
     def using_shortener(self):
         return self.features.get('using_shortener', 'N/A')
+    
+    @property
+    def url_length(self):
+        return self.features.get('url_length', 'N/A')
+    
+    @property
+    def num_subdomains(self):
+        return self.features.get('num_subdomains', 'N/A')
+    
+    @property
+    def contains_keyword(self):
+        return self.features.get('contains_keyword', 'N/A')
+    
+    @property
+    def has_ip(self):
+        return self.features.get('has_ip', 'N/A')
+    
 
     def __str__(self):
         return f"{self.url} -> {self.prediction} ({self.confidence:.2f})"
